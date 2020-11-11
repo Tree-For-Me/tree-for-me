6c4cbd29f3a4b1092670f5913f431e8a8f0f56d2
kurtz335 <kurtz.335@osu.edu>
2020-11-10 19:04:09 -0500

    disable input upon conversation ending

--- src/app/chatbox/chatbox.component.ts
+++ src/app/chatbox/chatbox.component.ts
@@ -53,35 +53,48 @@ export class ChatboxComponent implements OnInit {
 	var responseMessage: Message;
 	this.messagesService.getAssistantResponse(new Message(textStr, this.convoID)).subscribe((data) => {
 	    responseMessage = data;
-	    this.plantInfoService.plantInfoRequest(new Message("plantRetrieval", this.convoID)).subscribe((plants) => {
+
+	    this.messages.pop();
+	    var plantResultUser = 0;
+        if (responseMessage.user == -2) {
+            var button = <HTMLButtonElement> document.getElementById("submitButton");
+            var field = <HTMLButtonElement> document.getElementById("inputField");
+            button.disabled = true;
+            field.disabled = true;
+            this.messages.push(new ChatMessage("We found the plant for you! Please look to the right to find possible matches!\nIf you would like to find some new plants, please hit the reset button.", false));
+            plantResultUser = this.convoID;
+            this.convoID = -1;
+        } else {
+            this.convoID = responseMessage.user;
+            plantResultUser = this.convoID;
+            this.sendNextComputerMessage(responseMessage.messageContent);
+        }
+
+	    this.plantInfoService.plantInfoRequest(new Message("plantRetrieval", plantResultUser)).subscribe((plants) => {
                         console.log('PLANTS: ', plants);
                         // When the plant is found print it to the user
                         if (plants.length != 0) {
                             this.plantResults = plants;
                         } else {
-                            this.resetConversation();
+                            this.resetConversation(this.plantResults);
                             this.messages.push(new ChatMessage("Hmmm, it looks like no plants fit all your criteria. Click the button on the right to start over.", false));
                         }
                     })
-
-        this.messages.pop();
-	    if (responseMessage.user == -2) {
-            this.messages.push(new ChatMessage("We found the plant for you! Please look to the right to find possible matches!", false));
-	        this.convoID = -1;
-	    } else {
-            this.convoID = responseMessage.user;
-            this.sendNextComputerMessage(responseMessage.messageContent);
-	    }
 
 
 	});
 
   }
 
-  resetConversation() {
+  resetConversation(plants: Plant[]) {
     this.plantInfo = new PlantInfo('', '', false, false);
     this.messages = [];
-    this.plantResults = [];
+    this.plantResults = plants;
+
+    var button = <HTMLButtonElement> document.getElementById("submitButton");
+    var field = <HTMLButtonElement> document.getElementById("inputField");
+    button.disabled = false;
+    field.disabled = false;
 
     //welcome prompt
     this.messagesService.getAssistantResponse(new Message("", -1)).subscribe((data) => {
